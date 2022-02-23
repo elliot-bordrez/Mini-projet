7877ffcb06de280f318313e469efa77b35b22cc9
v-riquier <90181675+v-riquier@users.noreply.github.com>
2022-02-23 16:08:41 +0100

    Add files via upload

--- MQTT.ino
+++ MQTT.ino
@@ -1,48 +1,50 @@
 #include <Bridge.h>
-#include <BridgeSSLClient.h>
-#include <MQTT.h>
+#include <Console.h>
+#include <BridgeClient.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+#include <ArduinoJson.h>
 
-BridgeSSLClient net;
-MQTTClient client;
+/************************* Assetwolf Portal Setup *********************************/
 
-unsigned long lastMillis = 0;
+//Connection details found on the asset page
+#define SERVER "192.168.64.226"
+#define PORT 1883
+#define USERNAME "username"
+#define PASSWORD "password"
+#define DEVICE_ID "Xxs2h2"
 
-void connect() {
-  Serial.print("connecting...");
-  while (!client.connect("arduino", "public", "public")) {
-    Serial.print(".");
-    delay(1000);
-  }
+/************ Global State (you don't need to change this!) ******************/
 
-  Serial.println("\nconnected!");
+// Create a BridgeClient instance to communicate using the Yun's bridge & Linux OS.
+BridgeClient client;
 
-  client.subscribe("/hello");
-  // client.unsubscribe("/hello");
-}
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, SERVER, PORT, USERNAME, PASSWORD);
 
-void messageReceived(String &topic, String &payload) {
-  Serial.println("incoming: " + topic + " - " + payload);
-}
+/*************************** Sketch Code ************************************/
+int value;
+DynamicJsonBuffer doc(256);
+JsonObject& root = doc.createObject();
 
 void setup() {
   Bridge.begin();
-  Serial.begin(115200);
-
-  client.begin("public.cloud.shiftr.io", 8883, net);
-  client.onMessage(messageReceived);
-
-  connect();
+  Console.begin();
+  pinMode(6, INPUT);
+  root["type"] = "mouvement";
 }
 
 void loop() {
-  client.loop();
-
-  if (!client.connected()) {
-    connect();
+  value = digitalRead(6);
+  root["value"] = value;
+  if (value == 0) {
+    Console.println(F("Personne"));
+    root["unit"] = "OFF";
   }
-
-  if (millis() - lastMillis > 1000) {
-    lastMillis = millis();
-    client.publish("/hello", "world");
+  else {
+    Console.println(F("Repéré"));
+    root["unit"] = "ON";
   }
+  delay(1000);
+  root.prettyPrintTo(client);
 }
