46ad6cd2fc87a6173a78994e76b850ed1af53b83
v-riquier <90181675+v-riquier@users.noreply.github.com>
2022-03-22 09:31:35 +0100

    Add files via upload

--- MQTT.ino
+++ MQTT.ino
@@ -8,7 +8,7 @@
 /************************* Assetwolf Portal Setup *********************************/
 
 //Connection details found on the asset page
-#define SERVER "192.168.64.226"
+#define SERVER "192.168.65.104"
 #define PORT 1883
 #define USERNAME "username"
 #define PASSWORD "password"
@@ -27,9 +27,29 @@ int value;
 DynamicJsonBuffer doc(256);
 JsonObject& root = doc.createObject();
 
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Console.print("Connecting to MQTT... ");
+
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+    Console.println(mqtt.connectErrorString(ret));
+    Console.println("Retrying MQTT connection in 5 seconds...");
+    mqtt.disconnect();
+    delay(5000);  // wait 5 seconds
+  }
+  Console.println("MQTT Connected!");
+}
+
 void setup() {
   Bridge.begin();
   Console.begin();
+  mqtt.subscribe(&onoffbutton);
   pinMode(6, INPUT);
   root["type"] = "mouvement";
 }
@@ -38,12 +58,17 @@ void loop() {
   value = digitalRead(6);
   root["value"] = value;
   if (value == 0) {
-    Console.println(F("Personne"));
     root["unit"] = "OFF";
   }
   else {
-    Console.println(F("Repéré"));
     root["unit"] = "ON";
+  }
+  MQTT_connect();
+  Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription(1000))) {
+    if (subscription == &onoffbutton) {
+      
+    }
   }
   root.prettyPrintTo(Console);
   Console.println();
